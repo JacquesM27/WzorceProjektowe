42081358fd3f6115b1d190ee0b515d52e563abb3
s20034JM <80154434+s20034JM@users.noreply.github.com>
2022-10-06 08:26:13 +0200

    something went wrong last time

--- Composite2/Program.cs
+++ Composite2/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Composite2;
+
 Console.WriteLine("Hello, World!");
+
+
+Pte private1 = new("John", "private", 10);
+decimal salary = private1.CalculateBaseSalary();
+Console.WriteLine("Salary - " + salary.ToString());
+
+Lt lieutenant = new("Adam", "", 103);
+Lt lieutenant2 = new("Michael", "", 300);
+
+lieutenant2.Add(new Pte("Stan", "", 40));
+lieutenant2.Add(new Pte("Stan", "", 40));
+lieutenant2.Add(new Pte("Stan", "", 40));
+
+Lt lieutenant3 = new("Boss", "", 4000);
+lieutenant3.Add(new Pte("Jim", "", 30));
+lieutenant3.Add(new Pte("Jim", "", 40));
+lieutenant3.Add(new Pte("John", "", 50));
+
+lieutenant.Add(lieutenant3);
+lieutenant.Add(lieutenant2);
+
+decimal requiredBudget = lieutenant.CalculateBaseSalary();
+
+Console.WriteLine($"Required budget for this company - {requiredBudget}");
