09b4cb5614da1a53df503684ec4617653be1141e
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-04-19 21:31:38 +0200

    Update CreateProductCommandHandler.cs

--- Mediator2/Handlers/Products/CreateProductCommandHandler.cs
+++ Mediator2/Handlers/Products/CreateProductCommandHandler.cs
@@ -1,4 +1,6 @@
 ﻿using Mediator2.Data;
+using Mediator2.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 using SimpleSoft.Mediator;
 
 namespace Mediator2.Handlers.Products
@@ -11,9 +13,27 @@ namespace Mediator2.Handlers.Products
             _dbContext = dbContext;
         }
 
-        public Task<CreateProductResult> HandleAsync(CreateProductCommand cmd, CancellationToken ct)
+        public async Task<CreateProductResult> HandleAsync(CreateProductCommand cmd, CancellationToken ct)
         {
-            throw new NotImplementedException();
+            var products = _dbContext.Set<ProductEntity>();
+
+            if (await products.AnyAsync(c => c.Code == cmd.Code, ct))
+            {
+                throw new InvalidOperationException($"Product with code '{cmd.Code}' already exists");
+            }
+
+            var externalId = Guid.NewGuid();
+            await products.AddAsync(new ProductEntity
+            {
+                ExternalId = externalId,
+                Code = cmd.Code,
+                Name = cmd.Name,
+                Price = cmd.Price
+            }, ct);
+
+            await _dbContext.SaveChangesAsync(ct);
+
+            return new CreateProductResult { Id = externalId };
         }
     }
 }
