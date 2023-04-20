796f20ea9eeac157948ce37a136adbc81adf9dac
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-04-20 20:45:47 +0200

    add simple models

---
+++ Mediator3/Model/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Mediator3.Model
+{
+    public class OrderItem
+    {
+        public int ProductId { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Mediator3/Model/UnitOfMeasure.cs
@@ -0,0 +1,8 @@
+﻿namespace Mediator3.Model
+{
+    public class UnitOfMeasure
+    {
+        public int UnitId { get; set; }
+        public string Shortcut { get; set; } = string.Empty;
+    }
+}
