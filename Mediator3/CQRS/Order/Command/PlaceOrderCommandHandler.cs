5b60cf37488b071ca4ff20c581bfbc6ac652f0a3
Jakub <42150609+JacquesM27@users.noreply.github.com>
2023-04-20 20:53:59 +0200

    created command and command handler for place order

---
+++ Mediator3/CQRS/Order/Command/PlaceOrderCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Mediator3.DTO;
+using Mediator3.Mediator;
+
+namespace Mediator3.CQRS.Order.Command
+{
+    public class PlaceOrderCommandHandler : IRequestHandler<PlaceOrderCommand, PlaceOrderResult>
+    {
+        private readonly IMediator _mediator;
+
+        public PlaceOrderCommandHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task<PlaceOrderResult> Handle(PlaceOrderCommand request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
